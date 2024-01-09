4b121cee8e65711ea3873022ef4b26c2ca6eb068
ashton15 <ashton.c@redbus.com>
2024-01-09 23:42:47 +0530

    Your commit message

---
+++ swap1.java
@@ -0,0 +1,11 @@
+class Swap1
+{
+    public static void main(String args[])
+    {
+        int a = 10, b = 20;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.print(a+" "+b); 
+    }
+}
